20b568d9466ba26f84b663ef7e1c925500d15b4d
xiahoux <xiahoux.eyez@gmail.com>
2020-12-01 19:02:03 +0300

    Выполнена третья домашка!

--- src/ru/Main.java
+++ src/ru/Main.java
@@ -15,13 +15,8 @@ public class Main {
         System.out.println("Выбор задания зациклен, так что можно запускать один и тот же метод сколько угодно раз подряд");
         System.out.println("без перезапуска программы.");
         System.out.println("Меню заданий:");
-        System.out.println("1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;");
-        System.out.println("2. Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;");
-        System.out.println("3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;");
-        System.out.println("4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью цикла(-ов) заполнить его диагональные элементы единицами;");
-        System.out.println("5. ** Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета)");
-        System.out.println("6. ** Написать метод, в который передается не пустой одномерный целочисленный массив, метод должен вернуть true, если в массиве есть место, в котором сумма левой и правой части массива равны;");
-        System.out.println("7. **** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным, или отрицательным), при этом метод должен сместить все элементымассива на n позиций. Для усложнения задачи нельзя пользоваться вспомогательными массивами;");
+        System.out.println("1. Игра УГАДАЙКА. Числа от 0 до 9. 3 попытки. Возможность переиграть по выбору игрока.");
+        System.out.println("2. Игра УГАДАЙ СЛОВО. Заданный массив слов, бесконечное количество попыток. Вывод угаданных букв.");
         System.out.println("0. Выход.");
 
 
@@ -40,114 +35,152 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         if (yourChoiсe == 1) {
-            int[] arr = {1, 0, 0, 1, 1, 0, 1, 0, 0, 1};
-            System.out.println("Изначальный массив: " + Arrays.toString(arr));
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] == 1) arr[i] = 0;
-                else arr[i] = 1;
-            }
-            System.out.println("Получившийся массив: " + Arrays.toString(arr));
-
-        } else if(yourChoiсe == 2){
-            final int SIZE = 8;
-            int[] arr = new int[SIZE];
-            for(int i = 0; i < SIZE; i++){
-                if(i > 0) arr[i] = arr[i-1] + 3;
-                else arr[i] = i;
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
-        } else if(yourChoiсe == 3){
-            int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-            for(int i = 0; i < arr.length; i++){
-                if(arr[i] < 6) arr[i] = arr[i] * 2;
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
-        }else if(yourChoiсe == 4){
-            final int SIZE = 10;
-            int[][] arr = new int[SIZE][SIZE];
-            for(int i = 0; i < SIZE; i++){
-                for(int j = 0; j < SIZE; j++){
-                    if(j == i || j + i == SIZE - 1) arr[i][j] = 1;
-                    else arr[i][j] = 0;
-                    System.out.printf("%2d ", arr[i][j]);
-                }
-                System.out.println();
-            }
-        }else if(yourChoiсe == 5){
-            final int SIZE = 20;
-            int[] arr = new int[SIZE];
-            int min = 100;
-            int max = 0;
-            for(int i = 0; i < SIZE; i++){
-                arr[i] = (int)(Math.random() * 100);
-                System.out.print(arr[i] + " ");
-                if(arr[i] < min) min = arr[i];
-                if(arr[i] > max) max = arr[i];
-            }
-            System.out.println();
-            System.out.println("Максимум равен: " + max);
-            System.out.println("Минимум равен: " + min);
-        }else if(yourChoiсe == 6){
-            final int SIZE = 5;
-            int[] arr = new int[SIZE];
-            for(int i = 0; i < SIZE; i++){
-                arr[i] = (int)(Math.random() * 10);
-                System.out.print(arr[i] + " ");
-            }
-            boolean result = checkBalance(arr);
-            if(result) System.out.println("Баланс найден!");
-            else System.out.println("Баланс не найден.");
-        }else if(yourChoiсe == 7){
-            final int SIZE = 10;
-            int[] arr = new int[SIZE];
-            for(int i = 0; i < arr.length; i++){
-                arr[i] = (int)(Math.random() * 10);
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
-            System.out.print("Введите n: ");
-            int n = scanner.nextInt();
-            System.out.println(Arrays.toString(arr) + " - исходный массив");
-            arr = shift(arr, n);
-            System.out.println(Arrays.toString(arr) + " - массив после сдвига");
-
+            taskNumber1();
+        } else if (yourChoiсe == 2){
+            taskNumber2();
         }else if(yourChoiсe == 0) return 0;
         return 1;
+    }
 
-    }
-    private static boolean checkBalance(int[] arr) {
-        int sum1 = 0;
-        int sum2 = 0;
-        for(int i = 0; i < arr.length-1; i++){
-            sum1 += arr[i];
-            for(int j = i+1; j < arr.length; j++){
-                sum2 += arr[j];
-            }
-            if(sum1 == sum2) break;
-            sum2 = 0;
+
+    private static void taskNumber1() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("'УГАДАЙКА'");
+        System.out.println("Привет!");
+        System.out.println("В игре 'УГАДАЙКА' тебе предстоит угадать число от 0 до 9, загаданное компьютером.");
+        System.out.println("У тебя будет 3 попытки, израсходовав которые ты проиграешь и сможешь попробовать ещё раз.");
+
+        while(true){
+            if(guessGame());
+            else break;
         }
-        return sum1 == sum2;
     }
-    private static int[] shift(int[] arr, int n) {
-        if(n > 0){
-            for(int i = 1; i <= n; i++) {
-                int arrDrop = arr[arr.length-1];
-                for (int j = arr.length-1; j > 0; j--) {
-                    arr[j] = arr[j - 1];
+
+    private static void taskNumber2() {
+        String gameName = "УГАДАЙ СЛОВО";
+        System.out.printf("Привет! Это игра %s.%n", gameName);
+        System.out.println("Я загадаю одно из этих слов:");
+
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+
+        for(int i = words.length-1; i > 0; i--){ //сортировка массива по длине слова
+            for(int j = 0; j < i; j++){
+                if(words[j].length() > words[j+1].length()){
+                    String buff = words[j];
+                    words[j] = words[j+1];
+                    words[j+1] = buff;
                 }
-                arr[0] = arrDrop;
             }
-        }else if(n < 0){
-            for(int i = -1; i >= n; i--){
-                int arrDrop = arr[0];
-                for (int j = 0; j < arr.length-1; j++){
-                    arr[j] = arr[j + 1];
-                }
-                arr[arr.length - 1] = arrDrop;
+
+        }
+        for(int i = 0; i < words.length; i++){ //вывод массива на печать таблицей
+            if(i%5 == 0) {
+                System.out.println();
+                System.out.printf("%10s", words[i]);
             }
-        }else System.out.println("На ноль сдвигать нельзя :ъ");
-        return arr;
+            else System.out.printf("%10s", words[i]);
+        }
+
+        System.out.println();//отступы
+        System.out.println();
+
+        System.out.println("Попробуй угадать, какое слово я загадал.");
+        System.out.println("Если ты не угадаешь, но в наших с тобой словах будут одинаковы буквы на одинаковых местах - я покажу их тебе с:");
+
+        while (true) {
+            if(guessWord(words));//запуск игры
+            else break;
+
+        }
+    }
+
+
+    private static boolean guessGame() {
+        Scanner scanner = new Scanner(System.in);
+        int counter = 0;
+        int number = (int)(Math.random() * 10);
+        while (counter < 3){
+            counter++;
+
+            System.out.print("Угадай число: ");
+
+            int playerTry = scanner.nextInt();
+
+            if(playerTry == number){
+                System.out.println("Вы победили!");
+                break;
+            }else if(playerTry > number){
+                System.out.println("Загаданное число меньше.");
+            }else if(playerTry < number){
+                System.out.println("Загаданное число больше.");
+            }
+        }
+        System.out.println("Повторить игру ещё раз? y/n");
+        String repit = scanner.next();
+
+        return (repit.equals("y"));
+    }
+    private static boolean guessWord(String[] words) {
+        Scanner scanner = new Scanner(System.in);
+
+        String answer = words[(int)(Math.random() * (words.length-1))]; //загадывается слово
+        boolean exit = false; //условие выхода из метода
+        final int SIZE = 15; //размер массива, заполненного #
+        char[] answerArr = new char[SIZE]; //создание этого массива
+
+        for(int i = 0; i < SIZE; i++){ //заполнение массива "#"
+            answerArr[i] = 35;
+        }
+
+        while(!exit){
+
+            System.out.print("Введите слово: ");
+            String playerAnswer = scanner.next().toLowerCase(); //playerAnswer - введённое игроком слово
+
+            exit = Compare(answer, playerAnswer, answerArr, SIZE); //запуск метода сравнения
+
+        }
+
+        return false;
+    }
+
+    private static boolean Compare(String answer, String playerAnswer, char[] answerArr, int SIZE) {
+        if(playerAnswer.equals(answer)){ //если игрок угадал
+
+            System.out.println("Мои поздравления! Вы угадали!");
+            return true;
+
+        }else if(playerAnswer.length() > answer.length()){ //решение проблемы, в которой при условии playerAnswer.length() < answer.length() выдавалась ошибка в цикле проверки
+
+            System.out.print("Неправильно! Совпавшие буквы будут отображены здесь: ");
+
+            for(int i = 0; i < answer.length(); i++){
+                if(playerAnswer.charAt(i) == answer.charAt(i)) answerArr[i] = playerAnswer.charAt(i); //проверка совпадений букв в загаднном и введённых словах
+            }
+
+            for(int i = 0; i < SIZE; i++){
+                System.out.print(answerArr[i]); //вывод в консоль массива с угаданными буквами и решётками (#)
+            }
+
+            System.out.println();
+            return false;
+
+        }else{
+
+            System.out.print("Неправильно! Совпавшие буквы будут отображены здесь: ");
+
+            for(int i = 0; i < playerAnswer.length(); i++){
+                if(playerAnswer.charAt(i) == answer.charAt(i)) answerArr[i] = playerAnswer.charAt(i);
+            }
+
+            for(int i = 0; i < SIZE; i++){
+                System.out.print(answerArr[i]);
+            }
+
+            System.out.println();
+            return false;
+
+        }
     }
 }
